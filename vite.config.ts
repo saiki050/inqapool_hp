4d92e4bb81f738d62237bf93d7d0776c8a5c3b12
saiki_mac <chaigucci2@gmail.com>
2026-01-04 11:54:04 +0000

    metadata削除（未使用）、build後にもfont適用

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: './',
     server: {
       port: 3000,
       host: '127.0.0.1',
     },
     plugins: [react()],
-    define: {
-      __ENV__: JSON.stringify(mode),
-    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
